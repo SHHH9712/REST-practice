54f0188670944b24d226f12dc9eca0b97dc92878
CiciZhao02 <xingyz5@uw.edu>
2023-12-30 15:04:27 -0800

    add functions to add register, login, delete, update users with AUTH, connection to mongo DB

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -16,3 +18,15 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Server is running on port http://localhost:8080')
+});
+
+const MONGO_URL = 'mongodb+srv://shhh9712:mXfgH2GEDPf2asDV@cluster0.bq6yynf.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router())
